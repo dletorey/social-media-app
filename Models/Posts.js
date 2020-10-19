a2137968dc1598b427d51be8cb798faf52cfa0cc
Dave Letorey <daveletorey@gmail.com>
2020-10-19 15:43:53 +0100

    made the same as the example

--- Models/Posts.js
+++ Models/Posts.js
@@ -1,26 +1,26 @@
-const {model, Schema} =  require('mongoose');
+const { model, Schema } = require('mongoose');
 
 const postSchema = new Schema({
-    body: String,
-    username: String,
-    createdAt: Date,
-    comments: [
-        {
-            body: String,
-            username: String,
-            createdAt: Date
-        }
-    ],
-    likes: [
-        {
-            username: String,
-            createdAt: Date
-        }
-    ],
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
+  body: String,
+  username: String,
+  createdAt: String,
+  comments: [
+    {
+      body: String,
+      username: String,
+      createdAt: String
     }
+  ],
+  likes: [
+    {
+      username: String,
+      createdAt: String
+    }
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  }
 });
 
 module.exports = model('Post', postSchema);
--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
-const mongoose = require("mongoose");
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
 
-const Post = require('./Models/Posts');
+const Post = require('./models/Posts');
 const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: Date!
-        Username: String!
-    }
-    type Query {
-        getPosts: [Post]
-    }
-`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+`;
 
 const resolvers = {
-    Query: {
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts;
-            } catch(err){
-                throw new Error(err);
-            }
-        }
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
     }
-}
+  }
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers
 });
 
-mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        console.log("MongoDB Connected")
-        return server.listen({ port: 5000});
-    })
-    .then(res => {
-        console.log(`Server running at ${res.url}`)
-    })
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
