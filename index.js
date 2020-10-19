1dfe823d371c2ac8a8275c361b666438e0645d6d
Dave Letorey <daveletorey@gmail.com>
2020-10-19 13:01:25 +0100

    created the index.js file that starts up a server on port 5000

---
+++ index.js
@@ -0,0 +1,25 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+
+
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello World!'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen({ port: 5000})
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    })
