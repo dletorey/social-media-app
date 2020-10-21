808f4b36af7b9441b0cc9568c99ac3bfe0dce4f1
Dave Letorey <daveletorey@gmail.com>
2020-10-21 20:40:16 +0100

    created the Apollo client to drop into the page

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import ApolloProvider from './ApolloProvider';
+ReactDOM.render(ApolloProvider, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
