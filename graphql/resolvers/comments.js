a00a20b11d5f56b6a59696c73e1a5541c5a20cb4
Dave Letorey <daveletorey@gmail.com>
2020-10-21 16:36:54 +0100

    added the deleteComment mutation

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,11 +1,11 @@
-const { UserInputError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Posts');
 const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Mutation: {
         createComment: async (_, { postId, body }, context) =>{
-            const username = checkAuth(context); // this uses the context with checkAuth module to make sure the user has a valid token
+            const { username } = checkAuth(context); // this uses the context with checkAuth module to make sure the user has a valid token
             if(body.trim() === ''){ // checks to see if the body of the comment is empty
                 throw new UserInputError('Empty comment', {
                     errors: {
@@ -26,6 +26,24 @@ module.exports = {
                 return post; // returns post
             } else throw new UserInputError('Post can not be found');
         },
-        
+        deleteComment: async (_, { postId, commentId }, context) => {
+            const { username } = checkAuth(context);
+      
+            const post = await Post.findById(postId);
+      
+            if (post) {
+              const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+      
+              if (post.comments[commentIndex].username === username) {
+                post.comments.splice(commentIndex, 1);
+                await post.save();
+                return post;
+              } else {
+                throw new AuthenticationError('Action not allowed');
+              }
+            } else {
+              throw new UserInputError('Post not found');
+            }
+        }
     }
 }
