5b70c3c5d45b0993cd40c39d69a6c5b66f7225ae
Dave Letorey <daveletorey@gmail.com>
2020-10-21 15:02:58 +0100

    added a check to see if the comment is empty

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,9 +1,19 @@
- const Post = require('../../models/Posts');
+const { UserInputError } = require('apollo-server');
+const Post = require('../../models/Posts');
 
  module.exports = {
      Mutation: {
          createComment: async (_, { postId, body }, context) =>{
-             
+            const user = checkAuth(context); // this uses the context with checkAuth module to make sure the user has a valid token
+            if(body.trim() === ''){ // checks to see if the body of the comment is empty
+                throw new UserInputError('Empty comment', {
+                    errors: {
+                        body: 'Comment must not be empty'
+                    }
+                })
+            }
+
+            
          }
      }
  }
