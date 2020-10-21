ac0b8754a436e407fbb6434969bee6c1020c70e7
Dave Letorey <daveletorey@gmail.com>
2020-10-21 11:42:47 +0100

    created a utility module to check that a user has a valid token

---
+++ util/check-auth.js
@@ -0,0 +1,24 @@
+
+const { AuthenticationError } = require('apollo-server');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../config');
+
+module.exports = (context) => {
+    // context = { ... headers } this is an example of what context will return
+    const authHeader = context.req.headers.authorization; // this sets the authorization header to context
+    if(authHeader){
+        // if authHeader exists split it to get the token
+        const token = authHeader.split('Bearer ')[1]; // this takes the token from Bearer and sets it to token
+        if(token){
+            try{
+                // if the token exists we use verify to check it is valid
+                const user = jwt.verify(token, SECRET_KEY);
+                return user;   
+            } catch(err) {
+                throw new AuthenticationError('Invalid or Expired token')
+            }
+        }
+        throw new Error('Authentication token must be of format Bearer [token]')
+    }
+    throw new Error('Authentication header must be provided')
+}
