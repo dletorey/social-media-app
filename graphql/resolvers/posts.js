0fdfedb082ed70da2ef716e652e25b963b9e1806
Dave Letorey <daveletorey@gmail.com>
2020-10-21 16:38:01 +0100

    added the like post mutation, which tests if a users has previously liked a post and if so removes the like

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -56,6 +56,27 @@ module.exports = {
       } catch(err) {
         throw new Error(err);
       }
+    },
+    // like post here
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already likes, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError('Post not found');
     }
   }
 };
