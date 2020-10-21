4511b55ec30b2d9e69cf189bc87dbb3fc10f374f
Dave Letorey <daveletorey@gmail.com>
2020-10-21 14:36:25 +0100

    added the mutations to create/delete comments and likes

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -42,5 +42,8 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
