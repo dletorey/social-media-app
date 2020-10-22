cb8950ee3a1476ad89a4cf527c939ad25ec17e9f
Dave Letorey <daveletorey@gmail.com>
2020-10-22 07:36:46 +0100

    Created the app menubar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import MenuBar from './Components/MenuBar';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
 
 function App() {
   return (
-    <div><h1>Welcome to Dave Social plat</h1></div>
+    <Router>
+      <MenuBar/>
+      <Route exact path='/' component={Home}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/register" component={Register}/>
+    </Router>
   );
 }
 
