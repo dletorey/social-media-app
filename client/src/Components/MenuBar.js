db53d3de9759cadc8841db4ce42b22e9d3c2d71c
Dave Letorey <daveletorey@gmail.com>
2020-10-22 09:59:25 +0100

    made the active state based on the pathname

--- client/src/Components/MenuBar.js
+++ client/src/Components/MenuBar.js
@@ -3,12 +3,14 @@ import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 function MenuBar() {
-  const [activeItem, setActiveItem] = useState('home');
+  const pathname = window.location.pathname;
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
     return (
-        <Menu pointing secondary>
+        <Menu pointing secondary size="massive" color="red">
             <Menu.Item
             name='home'
             active={activeItem === 'home'}
