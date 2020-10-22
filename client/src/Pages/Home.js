ba315980dada661d10d7ebacd668e2b5d7d301d1
Dave Letorey <daveletorey@gmail.com>
2020-10-22 10:27:49 +0100

    created a query to grab the posts from DB

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 function Home() {
+    const { loading, data: { getPosts: posts} } = useQuery(FETCH_POSTS_QUERY);
+
+    if(data){
+        console.log(data);
+    }
     return (
         <div>
             <h1>Home Page</h1>
@@ -8,4 +15,18 @@ function Home() {
     )
 }
 
-export default Home;
+const FETCH_POSTS_QUERY = gql`
+    {
+        getPosts{
+            id body createdAt username likeCount commentCount
+            likes{
+                username createdAt
+            }
+            comments{
+                id username createdAt body
+            }
+        }
+    }
+`
+
+export default Home; 
