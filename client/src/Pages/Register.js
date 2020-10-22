b79841402b4518e6f90cac878ef382b1847f5427
Dave Letorey <daveletorey@gmail.com>
2020-10-22 15:37:29 +0100

    moved the onChange & onSubmit functions to a utility

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -3,32 +3,30 @@ import { Button, Form } from 'semantic-ui-react';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
+import { useForm } from '../util/hooks';
+
 function Register(props) {
     const [errors, setErrors] = useState({});
-    const [values, setValues] = useState({
+
+    const { onChange, onSubmit, values } = useForm(registerUser, {
         username: '',
         email: '',
         password: '',
         confirmPassword: ''
     })
-    const onChange = (event) => {
-        setValues({ ...values, [event.target.name]: event.target.value});
-    }
-
+    
     const [addUser, { loading }] = useMutation(REGISTER_USER, {
         update(_, result){
             console.log(result);
             props.history.push('/');
         },
         onError(err){
-            console.log(err.graphQLErrors[0].extensions.exception.errors);
             setErrors(err.graphQLErrors[0].extensions.exception.errors);
         },
         variables: values
     })
 
-    const onSubmit = (event) => {
-        event.preventDefault();
+    function registerUser(){
         addUser();
     }
 
