b2a9361abe627b6e2468e1c28f7e9ad707fa3640
Dave Letorey <daveletorey@gmail.com>
2020-10-22 18:03:54 +0100

    made the auth data persist with localStorage

--- client/src/Components/MenuBar.js
+++ client/src/Components/MenuBar.js
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
 function MenuBar() {
-  const pathname = window.location.pathname;
-  const path = pathname === '/' ? 'home' : pathname.substr(1);
-  const [activeItem, setActiveItem] = useState(path);
+    const { user, logout } = useContext(AuthContext);
+    const pathname = window.location.pathname;
+    const path = pathname === '/' ? 'home' : pathname.substr(1);
+    const [activeItem, setActiveItem] = useState(path);
 
-  const handleItemClick = (e, { name }) => setActiveItem(name);
+    const handleItemClick = (e, { name }) => setActiveItem(name);
 
-    return (
+    const menuBar = user ? (
+        <Menu pointing secondary size="massive" color="red">
+            <Menu.Item
+            name={user.username}
+            active
+            as={Link}
+            to="/"
+            />
+            <Menu.Menu position='right'>
+            <Menu.Item
+            name='logout'
+            onClick={logout}
+            />
+            </Menu.Menu>
+        </Menu>
+    ) : (
         <Menu pointing secondary size="massive" color="red">
             <Menu.Item
             name='home'
@@ -36,5 +54,6 @@ function MenuBar() {
             </Menu.Menu>
         </Menu>
     )
+    return menuBar;
 }
 export default MenuBar;
--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,4 +1,16 @@
 import React, { useReducer, createContext } from 'react';
+import jwtDecode from 'jwt-decode';
+
+const initialState = {user: null};
+if(localStorage.getItem("jwtToken")){
+    const decodedToken = jwtDecode(localStorage.getItem("jwtToken"));
+
+    if(decodedToken.exp * 1000 < Date.now()){
+        localStorage.removeItem('jwtToken');
+    } else {
+        initialState.user = decodedToken;
+    }
+}
 
 const AuthContext = createContext({
     user: null,
@@ -24,15 +36,17 @@ function authReducer(state, action) {
 }
 
 function AuthProvider(props){
-    const [state, dispatch] = useReducer(authReducer, {user: null});
+    const [state, dispatch] = useReducer(authReducer, initialState);
 
     function login(userData){
+        localStorage.setItem("jwtToken", userData.token);
         dispatch({
             type: 'LOGIN',
             payload: userData
         });
     }
     function logout(){
+        localStorage.removeItem("jwtToken");
         dispatch({ type: 'LOGOUT'});
     }
 
