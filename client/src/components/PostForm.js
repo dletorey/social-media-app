736238b82ca4c7ba0403e8d9e43648052b6d1baf
Dave Letorey <daveletorey@gmail.com>
2020-10-23 11:05:03 +0100

    created the beginnings of the create post form

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Form, Button, Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { useForm } from '../util/hooks'
+import { ValuesOfCorrectTypeRule } from 'graphql';
+
+function PostForm(){
+
+    return (
+        <Form onSubmit={onSubmit}>
+            <h2>Create new post:</h2>
+            <Form.Field>
+                <Form.Input
+                    placeholder="Type here…"
+                    name="body"
+                    onChange={onChange}
+                    value={ValuesOfCorrectTypeRule.body}
+                />
+                <Button type="submit" color="red">
+                    Create
+                </Button>
+            </Form.Field>
+        </Form>
+    )
+};
+
+export default PostForm
