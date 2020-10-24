316047f96775148d94f911e096d2c07bd39c5cfa
Dave Letorey <daveletorey@gmail.com>
2020-10-24 17:54:15 +0100

    fixed the margin on the trash icon and floated the button to the right

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -45,8 +45,8 @@ function PostCard({
           </Label>
         </Button>
         {user && user.username === username && (
-          <Button as="div" color="red" onClick={() => console.log('Delete post')}>
-            <Icon name="trash" />
+          <Button as="div" color="red" floated="right" onClick={() => console.log('Delete post')}>
+            <Icon name="trash" style={{ margin: 0}} />
           </Button>
         )}
       </Card.Content>
