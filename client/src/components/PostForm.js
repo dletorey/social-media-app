c84d1af4b80e6587511569db1875d3a89de2d1f5
Dave Letorey <daveletorey@gmail.com>
2020-10-24 17:30:26 +0100

    handled the error when a user tries to post an empty post

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -24,7 +24,10 @@ function PostForm() {
         },
       });
       values.body = '';
-    }
+    },
+    onError(err) {
+      return err;
+    },
   });
 
   function createPostCallback() {
@@ -32,20 +35,30 @@ function PostForm() {
   }
 
   return (
-    <Form onSubmit={onSubmit}>
-      <h2>Create a post:</h2>
-      <Form.Field>
-        <Form.Input
-          placeholder="Hi World!"
-          name="body"
-          onChange={onChange}
-          value={values.body}
-        />
-        <Button type="submit" color="teal">
-          Submit
-        </Button>
-      </Form.Field>
-    </Form>
+    <>
+      <Form onSubmit={onSubmit}>
+        <h2>Create a post:</h2>
+        <Form.Field>
+          <Form.Input
+            placeholder="Hi World!"
+            name="body"
+            onChange={onChange}
+            value={values.body}
+            error={error ? true : false}
+          />
+          <Button type="submit" color="teal">
+            Submit
+          </Button>
+        </Form.Field>
+      </Form>
+      {error && (
+        <div className="ui error message" style={{ marginBottom: 20 }}>
+          <ul className="list">
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
