ff2665bb38a551beda5370bf03d5ac7255335dda
Dave Letorey <daveletorey@gmail.com>
2020-10-24 17:01:12 +0100

    on createPost read the cache and write the new post to the cache

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,69 +1,72 @@
 import React from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import { Button, Form } from 'semantic-ui-react';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 
-import { useForm } from '../util/hooks'
+import { useForm } from '../util/hooks';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
+function PostForm() {
+  const { values, onChange, onSubmit } = useForm(createPostCallback, {
+    body: ''
+  });
 
-function PostForm(){
+  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    variables: values,
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY
+      });
+      data.getPosts = [result.data.createPost, ...data.getPosts];
+      proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
+      values.body = '';
+    }
+  });
 
-    const { values, onChange, onSubmit } = useForm(createPostCallback, {
-        body: ''
-    });
+  function createPostCallback() {
+    createPost();
+  }
 
-    const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
-        variables: values,
-        update(_, result){
-            console.log(result)
-            values.body = ''
-        }
-    }); 
-
-    function createPostCallback() {
-        createPost();
-    };
-
-    return (
-        <Form onSubmit={onSubmit}>
-            <h2>Create new post:</h2>
-            <Form.Field>
-                <Form.Input
-                    placeholder="Type here…"
-                    name="body"
-                    onChange={onChange}
-                    value={values.body}
-                />
-                <Button type="submit" color="red">
-                    Create
-                </Button>
-            </Form.Field>
-        </Form>
-    )
-};
+  return (
+    <Form onSubmit={onSubmit}>
+      <h2>Create a post:</h2>
+      <Form.Field>
+        <Form.Input
+          placeholder="Hi World!"
+          name="body"
+          onChange={onChange}
+          value={values.body}
+        />
+        <Button type="submit" color="teal">
+          Submit
+        </Button>
+      </Form.Field>
+    </Form>
+  );
+}
 
 const CREATE_POST_MUTATION = gql`
-    mutation createPost($body: String!){
-        createPost(body: $body){
-            id
-            body
-            createdAt
-            username
-            likeCount
-            likes {
-                id
-                username
-                createdAt
-            }
-            commentCount
-            comments {
-                id
-                username
-                createdAt
-                body
-            }
-        }
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      createdAt
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      likeCount
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+      commentCount
     }
+  }
 `;
 
-export default PostForm
+export default PostForm;
