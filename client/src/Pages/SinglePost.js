79ee4666947105a354c0094103ab493de140cf5f
Dave Letorey <daveletorey@gmail.com>
2020-10-25 11:44:41 +0000

    created the markup for the singlePost if the post has loaded or not

--- client/src/Pages/SinglePost.js
+++ client/src/Pages/SinglePost.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
+import { Button, Card, Grid, Icon, Image, Label, Transition } from 'semantic-ui-react';
+import moment from 'moment';
+
+import { AuthContext } from '../context/auth';
+import LikeButton from '../components/LikeButton';
 
 function SinglePost(props){
     const postId = props.match.params.postId;
+    const { user } = useContext(AuthContext);
     console.log(postId);
-    const {data: { getPost }} = useQuery(FETCH_POSTS_QUERY, {
+    const {data: { getPost }} = useQuery(FETCH_POST_QUERY, {
         variables: {
             postId
         }
     })
 
     let postMarkup;
-    if(!);
+    if(!getPost){
+        postMarkup = <p>Loading post…</p>
+    } else {
+        const { id, body, createdAt, username, comments, likes, likeCount, commentCount} = getPost;
+
+        postMarkup = (
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column width={2}>
+                        <Image
+                        src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+                        size="small"
+                        float="right"/>
+                    </Grid.Column>
+                    <Grid.Column width={10}>
+                        <Card fluid>
+                            <Card.Content>
+                                <Card.Header>{username}</Card.Header>
+                                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                                <Card.Description>{body}</Card.Description>
+                            </Card.Content>
+                            <hr/>
+                            <Card.Content extra>
+                                <LikeButton user={user} post={{ id, likeCount, likes }}/>
+                                <Button as="div" labelPosition="right" onClick={() => console.log('Comment on post!!')}>
+                                    <Button basic color="blue">
+                                        <Icon name='comment alternate outline' />
+                                    </Button>
+                                    <Label basic color="blue" pointing="left">
+                                        {commentCount}
+                                    </Label>
+                                </Button>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        )
+    };
 };
 
 const FETCH_POST_QUERY = gql`
