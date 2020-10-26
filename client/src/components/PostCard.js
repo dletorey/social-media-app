1b658784961f479ec7c31edd647602ef58f4afe7
Dave Letorey <daveletorey@gmail.com>
2020-10-26 16:36:00 +0000

    imported MyPopup and replaced the Popup

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -6,6 +6,7 @@ import moment from 'moment';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
 import DeleteButton from './DeleteButton';
+import MyPopup from '../util/MyPopup';
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes }
@@ -28,10 +29,7 @@ function PostCard({
       </Card.Content>
       <Card.Content extra>
         <LikeButton user={user} post={{ id, likes, likeCount }} />
-        <Popup
-          content="Comment on Post"
-          inverted
-          trigger={
+        <MyPopup content="Comment on Post">
           <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
             <Button color="blue" basic>
               <Icon name="comments" />
@@ -40,8 +38,7 @@ function PostCard({
               {commentCount}
             </Label>
           </Button>
-          }
-        />
+        </MyPopup>
         {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
